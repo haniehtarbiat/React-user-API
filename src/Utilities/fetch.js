9e0fc93cf25ee49c25e84713143da66bffc400f5
hanieh tarbiat <tarbiathanieh@gmail.com>
2019-06-22 20:44:36 +0430

    update url API

--- src/Utilities/fetch.js
+++ src/Utilities/fetch.js
@@ -1,7 +1,7 @@
 const Fetch = (pageNumber) => {
   const pageSize= 12;
   return fetch(
-    `http://5cbc116dfa84180014bdb224.mockapi.io/users?page=${pageNumber} &limit= ${pageSize}`,
+    `https://5cbc116dfa84180014bdb224.mockapi.io/users?page=${pageNumber} &limit= ${pageSize}`,
   )
     .then(results => results.json())
     .then(data => data);
